58fdfa099ce972d2ea96bce85925c620ee14e462
ana mirela <ana.mirela.irimia@gmail.com>
2015-12-29 01:55:37 +0200

    added mountains landscape

--- trees/src/Main.java
+++ trees/src/Main.java
@@ -12,12 +12,14 @@ import javax.swing.JPanel;
 
 public class Main extends JPanel {
 	static Tree t1, t2;
-
+	static Landscape l;
 	@Override
 	public void paint(Graphics g1) {
+		
 		Graphics2D g = (Graphics2D) g1;
+		l.draw(g);
 		g.setColor(new Color(93, 46, 16));
-
+		
 		t1.draw(g);
 		t2.draw(g);
 	}
@@ -30,18 +32,18 @@ public class Main extends JPanel {
 
 		t1 = new Tree(new Vector2(500, 900), randVal);
 		t2 = new Tree(new Vector2(700, 900), randVal);
-
+		l = new Landscape();
 		ImageUtils.drawToFile(1000, 1000, t1, t2);
 		
-		/*
-		JFrame f = new JFrame();
+
+		/*JFrame f = new JFrame();
 
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		f.setContentPane(new Main());
 		f.setSize(1000, 1000);
-		f.setVisible(true);
-		*/
+		f.setVisible(true);*/
+	
 		System.out.println(System.currentTimeMillis() - start);
 	}
 }
