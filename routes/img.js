6be58c72bb8c53f6c2c9b6953a3dec04f30e9602
Alin Mindroc <mindroc.alin@gmail.com>
2015-12-30 12:15:10 +0200

    add gitignore

--- routes/img.js
+++ routes/img.js
@@ -3,25 +3,18 @@ var router = express.Router();
 var exec = require('child_process').exec;
 var spawn = require('child_process').spawn;
 
-/* GET users listing. */
+router.get('/delete', function(req, res, next) {
+	var child = spawn('rm', ['-f', 'logic/*.png']);
+});
+
 router.get('/:id', function(req, res, next) {
 	if (req.params.id.length != 1){
 		res.send("must send a number between 0 and 4 as arg");
 	} else {
-		process.chdir('logic');
-		// exec('ls -l', function(error, stdout, stderr) {
-			// console.log(stdout);
-		// });
-		
-		var child = spawn('java', ['app.VanGogh', req.params.id]);
-/*
-		var cmd = 'cd logic; java app.VanGogh ' + req.params.id;
+		console.log('generating ' + req.params.id);
 
-		res.sendStatus(500);
-		exec(cmd, function(error, stdout, stderr) {
-			console.log(stdout);
-		});
-*/
+		process.chdir('logic');
+		var child = spawn('java', ['app.VanGogh', req.params.id]);
 	}
 });
 
